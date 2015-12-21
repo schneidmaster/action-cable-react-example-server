0d343da42712d0e74fb55e52d656f6ab8955361f
Zach Schneider <zach@schneid.io>
2015-12-21 14:49:17 -0600

    Set up simple chat channel

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,5 +1,10 @@
-# Be sure to restart your server when you modify this file. Action Cable runs in an EventMachine loop that does not support auto reloading.
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      # Create a new random ID for the user.
+      self.current_user = SecureRandom.hex(64)
+    end
   end
 end
